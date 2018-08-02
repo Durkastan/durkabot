ddc918223f3a8499040f16f9e3c260538b9d2b92
ChemicalInk <38759407+ChemicalInk@users.noreply.github.com>
2018-08-02 11:16:45 +0300

    Updated python to 3.7 and refactored dataclasses to use decorator (#11)
    * Updated python to 3.7 and refactored dataclasses to use decorator
    
    * Revert prefix

--- src/extensions/archive/archive_result.py
+++ src/extensions/archive/archive_result.py
@@ -1,24 +1,8 @@
+from attr import dataclass
+
+
+@dataclass
 class ArchiveResult:
-    def __init__(self, link, archive_date, cache_hit):
-        self.link = link
-        self.archive_date = archive_date
-        self.cache_hit = cache_hit
-
-    def __repr__(self):
-        return (f"ArchiveResult("
-                f"link='{self.link}', "
-                f"archive_date='{self.archive_date}', "
-                f"cache_hit={self.cache_hit}"
-                f")")
-
-    # TODO: DRY with BookData
-    def __eq__(self, other):
-        ret = True
-        # ensure member variables are equal
-        for k in [x for x, y in self.__dict__.items() if not x.startswith('_') or callable(y)]:
-            ret = ret and getattr(self, k) == getattr(other, k)
-
-        return ret
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
+    link: str
+    archive_date: str
+    cache_hit: bool

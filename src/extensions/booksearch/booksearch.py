c024277b8f7cda6bef074ff005f1010d58de06a1
ChemicalInk <aladdinalkhafaji@gmail.com>
2018-08-04 12:16:53 +0300

    Added documentation for bot commmands

--- src/extensions/booksearch/booksearch.py
+++ src/extensions/booksearch/booksearch.py
@@ -9,5 +9,17 @@ class BookSearch:
 
     @commands.command()
     async def booksearch(self, ctx, book_site: BookSite, *, query: str):
+        """
+        Search for a book in the given library
+
+        To see currently supported libraries, run the `booksearch libraries` command.
+
+        Args:
+            book_site: Name of library as returned in `booksearch libraries`
+            query: Search query for the book
+
+        Returns:
+            Search results with direct links and links on library site
+        """
         embed = await book_site.search(query)
         await ctx.send(embed=embed)

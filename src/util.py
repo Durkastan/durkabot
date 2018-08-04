ae6cc98b0389c8866d3538b824ffc70b2ed6702b
Marty <40224433+martomato@users.noreply.github.com>
2018-08-04 14:28:35 -0700

    oops forgot space

--- src/util.py
+++ src/util.py
@@ -2,6 +2,6 @@ import os
 
 def get_prefix(bot, message):
     """TODO: Dynamic prefix for each server"""
-    prefix_list = ['d ', 'D ', 'durka ', 'Durka']
+    prefix_list = ['d ', 'D ', 'durka ', 'Durka ']
 
     return os.environ.get('PREFIX', prefix_list)

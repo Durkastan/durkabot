486fec56601648d68e38d17724e1b2b5820abaf5
Marty <40224433+martomato@users.noreply.github.com>
2018-08-04 14:15:49 -0700

    add command prefix synonyms

--- src/util.py
+++ src/util.py
@@ -1,6 +1,11 @@
 import os
 
-
 def get_prefix(bot, message):
     """TODO: Dynamic prefix for each server"""
-    return os.environ.get('PREFIX', "durka ")
+    return [
+            'd ',
+            'D ',
+            'durka ',
+            'Durka ',
+            os.environ.get('PREFIX')
+    ]

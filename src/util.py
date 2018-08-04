4ff4bae7876d5ea3ecb9839b17f2c8d1223cc96e
Marty <40224433+martomato@users.noreply.github.com>
2018-08-04 14:32:13 -0700

    add command prefix synonyms (#24)
    * add command prefix synonyms
    
    * hotfix to make sure we are calling environ-get right

--- src/util.py
+++ src/util.py
@@ -1,6 +1,7 @@
 import os
 
-
 def get_prefix(bot, message):
     """TODO: Dynamic prefix for each server"""
-    return os.environ.get('PREFIX', "durka ")
+    prefix_list = ['d ', 'D ', 'durka ', 'Durka ']
+
+    return os.environ.get('PREFIX', prefix_list)

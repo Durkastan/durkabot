ae5226ffac903be90dd99191cc43c2e6f3625dfc
ChemicalInk <aladdinalkhafaji@gmail.com>
2018-08-07 00:40:12 +0300

    Simple purge command

--- src/extensions/mod/mod.py
+++ src/extensions/mod/mod.py
@@ -31,3 +31,18 @@ class Mod:
         """
         for member in members:
             await ctx.guild.kick(member)
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    @commands.bot_has_permissions(manage_messages=True)
+    async def purge(self, ctx, limit: int = 100, *members: discord.Member):
+        """
+        DELET
+
+        Args:
+            limit: The number of messages to search through.
+            *members: If supplied, only messages from those users will
+                    be deleted. Otherwise, all messages are fair game.
+        """
+        check = (lambda m: m.author in members) if members else None
+        await ctx.channel.purge(limit=limit, check=check)

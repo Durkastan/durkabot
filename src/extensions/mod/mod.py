8dadd8a6b1edb1960818758f759e5f87a7e103da
ChemicalInk <aladdinalkhafaji@gmail.com>
2018-08-06 23:55:49 +0300

    Simple ban command

--- src/extensions/mod/mod.py
+++ src/extensions/mod/mod.py
@@ -1,3 +1,20 @@
+import discord
+from discord.ext import commands
+
+
 class Mod:
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    @commands.bot_has_permissions(ban_members=True)
+    async def ban(self, ctx, *members: discord.Member):
+        """
+        Swing your mighty Durkastani-made banhammer!
+
+        Args:
+            members: A list of users. Just mention!
+        """
+        for member in members:
+            await ctx.guild.ban(member)

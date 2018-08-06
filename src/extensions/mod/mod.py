84b663096906205faddc25c2c178bbdd8fe7c1c9
ChemicalInk <aladdinalkhafaji@gmail.com>
2018-08-06 23:59:47 +0300

    Simple kick command

--- src/extensions/mod/mod.py
+++ src/extensions/mod/mod.py
@@ -18,3 +18,16 @@ class Mod:
         """
         for member in members:
             await ctx.guild.ban(member)
+
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    @commands.bot_has_permissions(kick_members=True)
+    async def kick(self, ctx, *members: discord.Member):
+        """
+        Polish your boot for this one.
+
+        Args:
+            members: A list of users. Just mention!
+        """
+        for member in members:
+            await ctx.guild.kick(member)

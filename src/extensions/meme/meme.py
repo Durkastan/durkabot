23f174e89ae1668f41fb8cc7c1a8b80c80be0879
ChemicalInk <aladdinalkhafaji@gmail.com>
2018-11-22 14:39:39 +0300

    fix bug

--- src/extensions/meme/meme.py
+++ src/extensions/meme/meme.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 
+from common.mention_free_text import MentionFreeText
 from extensions.meme.psst import format_psst
 
 
@@ -8,7 +9,7 @@ class Meme:
         self.bot = bot
 
     @commands.command()
-    async def psst(self, ctx, *, txt: str):
+    async def psst(self, ctx, *, txt: MentionFreeText):
         """hey kid!"""
         processed = format_psst(txt)
         await ctx.send(processed)
--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,15 @@ import sys
 if 'tests' not in os.getcwd():
     os.chdir('./tests/')
     sys.path.append('../src/')
+
+from unittest.mock import MagicMock
+
+import pytest
+from discord.ext.commands import Context
+
+from main import Bot
+
+
+@pytest.fixture
+def ctx():
+    Context(prefix='durka', bot=MagicMock(spec=Bot), message=MagicMock())

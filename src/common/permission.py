4ada75626952bf66376d96c254ca0e9ada9d33cc
ChemicalInk <38759407+ChemicalInk@users.noreply.github.com>
2019-01-03 13:42:19 +0300

    Role command (#34)
    * Redesign quran embed
    
    * Role command
    
    * Role command supports multi word roles
    
    * bot cleans up mod-related messages
    
    * Role command now toggles adding a given role

---
+++ src/common/permission.py
@@ -0,0 +1,19 @@
+from discord.ext.commands import check, MissingPermissions
+
+
+def has_permissions(**perms):
+    def predicate(ctx):
+        ch = ctx.channel
+        permissions = ch.permissions_for(ctx.author)
+
+        if permissions.administrator or ctx.guild.owner == ctx.author:
+            return True
+
+        missing = [perm for perm, value in perms.items() if getattr(permissions, perm, None) != value]
+
+        if not missing:
+            return True
+
+        raise MissingPermissions(missing)
+
+    return check(predicate)
--- src/extensions/mod/mod.py
+++ src/extensions/mod/mod.py
@@ -1,13 +1,15 @@
 import discord
 from discord.ext import commands
 
+from common.permission import has_permissions
+
 
 class Mod:
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    @commands.has_permissions(ban_members=True)
+    @has_permissions(ban_members=True)
     @commands.bot_has_permissions(ban_members=True)
     async def ban(self, ctx, *members: discord.Member):
         """
@@ -19,12 +21,12 @@ class Mod:
         if members:
             for member in members:
                 await ctx.guild.ban(member)
-            await ctx.send("Banned.")
+            await ctx.send("Banned.", delete_after=5)
         else:
-            await ctx.send("Whaddaya mean, 'just ban anyone?'")
+            await ctx.send("Whaddaya mean, 'just ban anyone?'", delete_after=5)
 
     @commands.command()
-    @commands.has_permissions(kick_members=True)
+    @has_permissions(kick_members=True)
     @commands.bot_has_permissions(kick_members=True)
     async def kick(self, ctx, *members: discord.Member):
         """
@@ -36,12 +38,12 @@ class Mod:
         if members:
             for member in members:
                 await ctx.guild.kick(member)
-            await ctx.send('Booted.')
+            await ctx.send('Booted.', delete_after=5)
         else:
-            await ctx.send("Sorry chief, can't boot.")
+            await ctx.send("Sorry chief, can't boot.", delete_after=5)
 
     @commands.command()
-    @commands.has_permissions(manage_messages=True)
+    @has_permissions(manage_messages=True)
     @commands.bot_has_permissions(manage_messages=True)
     async def purge(self, ctx, limit: int = 100, *members: discord.Member):
         """
@@ -54,3 +56,25 @@ class Mod:
         """
         check = (lambda m: m.author in members) if members else None
         await ctx.channel.purge(limit=limit, check=check)
+
+    @commands.command()
+    @has_permissions(manage_roles=True)
+    @commands.bot_has_permissions(manage_roles=True)
+    async def role(self, ctx, member: discord.Member, *, role_name):
+        """
+        Add/remove a role to the given member
+        Args:
+            member: The member to add the role to
+            role_name: Role name, case-insensitive
+        """
+        role = discord.utils.find(lambda r: r.name.lower() == role_name, member.guild.roles)
+        if role not in member.roles:
+            await member.add_roles(role)
+        else:
+            await member.remove_roles(role)
+
+    async def __after_invoke(self, ctx):
+        await ctx.message.delete()
+
+    async def on_command_error(self, ctx, exception):
+        await ctx.message.delete()

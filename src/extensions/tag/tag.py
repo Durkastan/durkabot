4297413a7938802efb946be3515a01e7311a9fd1
ChemicalInk <aladdinalkhafaji@gmail.com>
2019-01-18 00:45:31 +0300

    Simple tag command

--- src/extensions/tag/tag.py
+++ src/extensions/tag/tag.py
@@ -4,20 +4,20 @@ from storage import StorageHandler
 
 
 class Tag:
-    db = 'tags'
+    collection = 'tags'
 
     def __init__(self, bot):
         self.bot = bot
         self.storage = StorageHandler()
 
     @commands.group(invoke_without_command=True)
+    @commands.has_permissions(kick_members=True)
     async def tag(self, ctx, key):
-        self.storage.feed(ctx)
-        tag = self.storage.db[self.db].find_one({'key': key})
+        tag = self.storage.db[self.collection].find_one({'key': key})
         await ctx.send(tag['value'])
 
     @tag.command()
-    async def add(self, ctx, key: str, value: str):
-        self.storage.feed(ctx)
-        self.storage.db[self.db].replace_one({'key': key}, {'key': key, 'value': value}, upsert=True)
+    @commands.has_permissions(kick_members=True)
+    async def create(self, ctx, key: str, *, value: str):
+        self.storage.db[self.collection].replace_one({'key': key}, {'key': key, 'value': value}, upsert=True)
         await ctx.send(f'Tag {key} created.')
--- src/storage.py
+++ src/storage.py
@@ -4,32 +4,6 @@ from pymongo import MongoClient
 
 
 class StorageHandler:
-    _client = None
-
-    def __init__(self):
-        self.database_name = ''
-        self._db = None
-
-    def feed(self, ctx):
-        self.database_name = f'db_{ctx.guild.id}'
-
-    @property
-    def client(self):
-        return self._get_client()
-
-    @classmethod
-    def _get_client(cls):
-        if cls._client is None:
-            cls._client = MongoClient(os.getenv("MONGOURL"))
-        return cls._client
-
-    @property
-    def db(self):
-        if self._db is None:
-            self._db = self._get_db()
-        return self._db
-
-    def _get_db(self):
-        db = self.client[self.database_name]
-        db.authenticate(name=os.getenv("MONGO_USERNAME"), password=os.getenv("MONGO_PASSWORD"))
-        return db
+    client = MongoClient(os.getenv("MONGOURL"))
+    database_name = "durkadb"
+    db = client[database_name]

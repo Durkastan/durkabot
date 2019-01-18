f3f1710dc797cc33bd3648e607202f599f1df040
ChemicalInk <aladdinalkhafaji@gmail.com>
2019-01-18 15:51:48 +0300

    Comments for tag cog

--- src/extensions/tag/tag.py
+++ src/extensions/tag/tag.py
@@ -13,20 +13,38 @@ class Tag:
         self.collection = self.storage.db[self.collection_name]
 
     @commands.group(invoke_without_command=True)
-    @commands.has_permissions(kick_members=True)
     async def tag(self, ctx, *, key):
+        """
+        Return text stored under tag.
+
+        Args:
+            key: Tag name. See the "tag list" subcommand
+        """
         tag = self.collection.find_one({'key': key})
         await ctx.send(tag['value'])
 
     @tag.command()
     @commands.has_permissions(kick_members=True)
     async def create(self, ctx, key: str, *, value: str):
+        """
+        Create a new tag.
+
+        Args:
+            key: Tag name. If it contains spaces, put it in quotes.
+            value: Tag value.
+        """
         self.collection.replace_one({'key': key}, {'key': key, 'value': value}, upsert=True)
         await ctx.send(f'Tag `{key}` created.')
 
     @tag.command()
     @commands.has_permissions(kick_members=True)
     async def delete(self, ctx, *, key):
+        """
+        Delete a tag.
+
+        Args:
+            key: Tag name.
+        """
         result = self.collection.delete_one({'key': key})
         if result.deleted_count != 0:
             await ctx.send(f'Tag `{key}` deleted.')
@@ -34,8 +52,10 @@ class Tag:
             await ctx.send(f'No tag `{key}`.')
 
     @tag.command()
-    @commands.has_permissions(kick_members=True)
     async def list(self, ctx):
+        """
+        List stored tags.
+        """
         cursor = self.collection.find(projection={'key': True})
         cursor.sort('key', pymongo.ASCENDING)
         tag_names = "\n".join([tag['key'] for tag in cursor])

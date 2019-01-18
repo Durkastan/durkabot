8cbd0b6325a38049a642cb7df6b5a7e31bec9faa
ChemicalInk <aladdinalkhafaji@gmail.com>
2019-01-18 15:25:01 +0300

    Polish tags cog

--- src/extensions/tag/tag.py
+++ src/extensions/tag/tag.py
@@ -1,23 +1,42 @@
+import pymongo
 from discord.ext import commands
 
 from storage import StorageHandler
 
 
 class Tag:
-    collection = 'tags'
+    collection_name = 'tags'
 
     def __init__(self, bot):
         self.bot = bot
         self.storage = StorageHandler()
+        self.collection = self.storage.db[self.collection_name]
 
     @commands.group(invoke_without_command=True)
     @commands.has_permissions(kick_members=True)
-    async def tag(self, ctx, key):
-        tag = self.storage.db[self.collection].find_one({'key': key})
+    async def tag(self, ctx, *, key):
+        tag = self.collection.find_one({'key': key})
         await ctx.send(tag['value'])
 
     @tag.command()
     @commands.has_permissions(kick_members=True)
     async def create(self, ctx, key: str, *, value: str):
-        self.storage.db[self.collection].replace_one({'key': key}, {'key': key, 'value': value}, upsert=True)
-        await ctx.send(f'Tag {key} created.')
+        self.collection.replace_one({'key': key}, {'key': key, 'value': value}, upsert=True)
+        await ctx.send(f'Tag `{key}` created.')
+
+    @tag.command()
+    @commands.has_permissions(kick_members=True)
+    async def delete(self, ctx, *, key):
+        result = self.collection.delete_one({'key': key})
+        if result.deleted_count != 0:
+            await ctx.send(f'Tag `{key}` deleted.')
+        else:
+            await ctx.send(f'No tag `{key}`.')
+
+    @tag.command()
+    @commands.has_permissions(kick_members=True)
+    async def list(self, ctx):
+        cursor = self.collection.find(projection={'key': True})
+        cursor.sort('key', pymongo.ASCENDING)
+        tag_names = "\n".join([tag['key'] for tag in cursor])
+        await ctx.send(f'```\n{tag_names}```')

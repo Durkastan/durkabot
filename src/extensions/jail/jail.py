3b10fde43035e64184285e283f38133ef265e657
ChemicalInk <aladdinalkhafaji@gmail.com>
2019-04-26 23:10:09 +0300

    Better separate responsibilities on jail cog

--- src/extensions/jail/jail.py
+++ src/extensions/jail/jail.py
@@ -12,9 +12,22 @@ class Jail(Cog):
         self.jailer = Jailer()
 
     async def jail(self, ctx, *members: discord.Member):
+        jail_role = ctx.guild.get_role(self.JAIL_ROLE_ID)
+
         for member in members:
-            await self.jailer.jail(member, discord.utils.get(ctx.guild.roles, id=self.JAIL_ROLE_ID))
+            old_roles = member.roles
+            self.jailer.jail(member)
+
+            await member.add_roles(jail_role)
+            await member.remove_roles(old_roles)
 
     async def unjail(self, ctx, *members: discord.Member):
+        jail_role = ctx.guild.get_role(self.JAIL_ROLE_ID)
+
         for member in members:
-            await self.jailer.unjail(member, discord.utils.get(ctx.guild.roles, id=self.JAIL_ROLE_ID))
+            ctx.guild.get_role()
+            role_ids = self.jailer.unjail(member)
+            roles = [ctx.guild.get_role(role_id) for role_id in role_ids]
+
+            await member.add_roles(roles)
+            await member.remove_roles(jail_role)
--- src/extensions/jail/jailer.py
+++ src/extensions/jail/jailer.py
@@ -8,39 +8,40 @@ class Jailer:
         self.db = StorageHandler.db
         self.collection = self.db[self.collection_name]
 
-    async def jail(self, member, jail_role):
+    def jail(self, member):
         doc = self.collection.find_one({'member_id': member.id})
-        doc = await self._jail(doc, member, jail_role)
+        doc = self._jail(doc, member)
         self.collection.replace_one({'member_id': member.id}, doc, upsert=True)
 
-    async def _jail(self, doc, member, jail_role):
+    def _jail(self, doc, member):
         doc = doc or self.default_doc(member.id)
 
         if doc['is_jailed'] is True:
             return doc
 
-        doc['roles'] = member.roles
+        doc['roles'] = [role.id for role in member.roles]
         doc['is_jailed'] = True
-        await member.add_roles(jail_role)
-        await member.remove_roles(doc['roles'])
 
         return doc
 
-    async def unjail(self, member, jail_role):
+    def unjail(self, member):
         doc = self.collection.find_one({'member_id': member.id})
-        doc = await self._unjail(doc, member, jail_role)
+        doc, roles = self._unjail(doc, member)
         self.collection.replace_one({'member_id': member.id}, doc, upsert=True)
 
-    async def _unjail(self, doc, member, jail_role):
+        return roles
+
+    def _unjail(self, doc, member):
         doc = doc or self.default_doc(member.id)
 
         if doc['is_jailed'] is False:
             return doc
 
-        await member.add_roles(doc['roles'])
-        await member.remove_roles(jail_role)
+        old_roles = doc['roles']
+        doc['roles'] = []
+        doc['is_jailed'] = False
 
-        return doc
+        return doc, old_roles
 
     @staticmethod
     def default_doc(member_id):
